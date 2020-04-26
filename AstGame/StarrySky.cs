5949d2ee0e7482db914fcac5d6592f3c13fb7a82
Максим-1987 <max-tenigin1987@mail.ru>
2020-04-26 11:06:25 +0300

    Рефакторинг

--- AstGame/StarrySky.cs
+++ AstGame/StarrySky.cs
@@ -16,28 +16,12 @@ namespace AsteroidGame
         }
         public override void Draw(Graphics g)
         {
-
             g.DrawLine(Pens.Red, _Position.X, _Position.Y, _Position.X + _Size.Width, _Position.Y + _Size.Height);
-            g.DrawLine(Pens.Red, _Position.X + _Size.Width, _Position.Y, _Position.X, _Position.Y + _Size.Height);
-
-            //g.DrawLine(Pens.Red, _Position.X, _Position.Y, _Position.X + _Size.Width / 2, _Position.Y + _Size.Height / 2);
-            //g.DrawLine(Pens.Red, _Position.X + _Size.Width / 2, _Position.Y + _Size.Height / 2, _Position.X, _Position.Y + _Size.Height / 2);
-            //g.DrawLine(Pens.Red, _Position.X, _Position.Y, _Position.X, _Position.Y + _Size.Height);
+            g.DrawLine(Pens.Red, _Position.X + _Size.Width, _Position.Y, _Position.X, _Position.Y + _Size.Height);          
         }
 
         public override void Update()
         {
-            //_Position.X += _Direction.X;
-            //_Position.Y += _Direction.Y;
-            //if (_Position.Y < 0)
-            //    _Direction.Y = -_Direction.Y;
-            //if (_Position.X < 0)
-            //    _Direction.X = -_Direction.X;
-            //if (_Position.Y > Game.Height)
-            //    _Direction.Y = -_Direction.Y;
-            //if (_Position.X > Game.Width)
-            //    _Direction.X = -_Direction.X;
-
             _Position.Y += _Direction.Y;
             if (_Position.Y < 0)
                 _Position.Y = Game.Height + _Size.Height;
