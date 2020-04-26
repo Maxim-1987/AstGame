0622f5bbafe7d379f7f280e85506e0a90ff122e9
Максим-1987 <max-tenigin1987@mail.ru>
2020-04-26 11:58:18 +0300

    Добавил код с урока

--- AstGame/VisualObject/Asteroid.cs
+++ AstGame/VisualObject/Asteroid.cs
@@ -12,8 +12,8 @@ namespace AsteroidGame.VisualObjects
         //private static readonly Image __Image = Image.FromFile("src\\Ast.png");
 
         public Asteroid(Point Position, Point Direction, int ImageSize)
-            //: base(Position, Direction, new Size(ImageSize, ImageSize), __Image)
-            : base(Position, Direction, new Size(ImageSize, ImageSize), Properties.Resources.Ast)
+        //: base(Position, Direction, new Size(ImageSize, ImageSize), __Image)
+        : base(Position, Direction, new Size(ImageSize, ImageSize), Properties.Resources.Ast)
         {
         }
 
