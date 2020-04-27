c0e17498bc107b10bc46600c5f95b091b8ab650b
Максим-1987 <max-tenigin1987@mail.ru>
2020-04-27 11:04:14 +0300

    Создан класс массив работников

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -9,16 +9,9 @@ namespace Employee
     class Program
     {       
         static void Main(string[] args)
-        {            
-            BaseEmployee[] workers = new BaseEmployee[4];
-            workers[0] = new HourlyPayment(85, 1, "Максим");
-            workers[1] = new FixedPayment(10000, 2, "Сергей");
-            workers[2] = new HourlyPayment(60, 1, "Юрий");
-            workers[3] = new FixedPayment(25000, 2, "Светлана");            
-            foreach (BaseEmployee worker in workers)
-            {
-                worker.PaymentCalculation();
-            }
+        {
+            Array ar = new Array();
+            ar.ArrayEmployee();
 
             Console.WriteLine("Введите Ваше имя: ");
             string name = Console.ReadLine();
