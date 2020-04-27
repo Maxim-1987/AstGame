e1aad39485bf49c9635070bf1e5783dfb10e002a
Максим-1987 <max-tenigin1987@mail.ru>
2020-04-27 11:13:16 +0300

    Добавил класс выбора ставки

---
+++ Employee/EmployeeInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+   public class EmployeeInput
+    {
+         public void EmployeeIn()
+        {
+            Console.WriteLine("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите Ваш id: 1 - часовая ставка или 2 - фиксированная ставка");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите Вашу ставку:");
+            int rate = int.Parse(Console.ReadLine());
+            if (id == 1)
+            {
+                HourlyPayment hp = new HourlyPayment(rate, id, name);
+                hp.PaymentCalculation();
+            }
+            if (id == 2)
+            {
+                FixedPayment fp = new FixedPayment(rate, id, name);
+                fp.PaymentCalculation();
+            }
+        }
+    }
+}
--- Employee/Program.cs
+++ Employee/Program.cs
@@ -12,23 +12,10 @@ namespace Employee
         {
             Array ar = new Array();
             ar.ArrayEmployee();
-
-            Console.WriteLine("Введите Ваше имя: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Введите Ваш id: 1 или 2");
-            int id = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите Вашу ставку:");
-            int rate = int.Parse(Console.ReadLine());
-            if (id == 1)
-            {
-                HourlyPayment hp = new HourlyPayment(rate, id, name);
-                hp.PaymentCalculation();
-            }
-            if (id == 2)
-            {
-                FixedPayment fp = new FixedPayment(rate, id, name);
-                fp.PaymentCalculation();
-            }
+            
+            EmployeeInput ei = new EmployeeInput();
+            ei.EmployeeIn();
+            
             Console.ReadLine();            
         } 
     }
