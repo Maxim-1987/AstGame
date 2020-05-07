3e0e13876e85e5b3102c417dec8c04c705c47449
Максим-1987 <max-tenigin1987@mail.ru>
2020-05-07 13:15:19 +0300

    Обращение к OrderBy с использованием лямбда-выражения

---
+++ Test/Collection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Collection
+    {
+        internal void NumberRepetitions()
+        {
+            var list = new List<string>();
+            Random rnd = new Random();
+            for (var i = 0; i < 10; i++)
+                list.Add(rnd.Next(1, 5).ToString());
+
+            var dict = new Dictionary<string, int>();
+            foreach (var item in list)
+            {
+                if (!dict.ContainsKey(item))
+                    dict.Add(item, 1);
+                else
+                    dict[item]++;
+            }
+            foreach (var point in dict)
+                Console.WriteLine($"строка: {point.Key}, встречается {point.Value} раз");
+            Console.WriteLine("_______________________________________");
+
+            var result = dict.Select(r => new { Value = r.Value, Key = r.Key });
+            foreach (var item in result)
+                Console.WriteLine($"строка: {item.Key as string}, встречается {item.Value} раз");
+        }
+        
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -8,27 +8,15 @@ namespace Test
 {
     class Program
     {
+        private static Lambda __Lambda = new Lambda();
+        private static Collection __Collection = new Collection();
+
         static void Main(string[] args)
-        {
-            var list = new List<string>();
-            Random rnd = new Random();
-            for (var i = 0; i < 10; i++)
-                list.Add(rnd.Next(1, 5).ToString());
-
-            var dict = new Dictionary<string, int>();
-            foreach (var item in list)
-            {
-                if (!dict.ContainsKey(item))
-                    dict.Add(item, 1);
-                else
-                    dict[item]++;
-            }
-            foreach (var point in dict)
-                Console.WriteLine($"строка: {point.Key}, встречается {point.Value} раз");
-
-            var result = dict.Select(r => new { Value = r.Value, Key = r.Key });
-            foreach (var item in result)
-                Console.WriteLine($"строка: {item.Key as string}, встречается {item.Value} раз");
+        {                        
+            __Lambda.MethodLambda();
+            Console.WriteLine("_______________________________________");
+            __Collection.NumberRepetitions();
+            
             Console.ReadLine();
         }
     }
