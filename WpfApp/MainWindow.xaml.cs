c30790079e221fedfb7c7eaac279ce4cf503644f
Максим-1987 <max-tenigin1987@mail.ru>
2020-05-10 16:58:58 +0300

    Главное окно приложения

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -20,9 +20,17 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+         
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
