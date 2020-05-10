1c12b042c2778a14d55721e2ff815ca343e14259
Максим-1987 <max-tenigin1987@mail.ru>
2020-05-10 17:33:19 +0300

    Добавлены 4 департамента

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -18,19 +18,48 @@ namespace WpfApp
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
+    public class Departament
+    {        
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+        public override string ToString()
+        {
+            return $"Name:{Name}, Employees Count:{Employees.Count}";
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public override string ToString()
+        {
+            return $"Id:{Id}, Name:{Name}";
+        }
+    }
+
     public partial class MainWindow : Window
     {
-         
+        public List<Departament> Departaments = new List<Departament>();
+        public List<Employee> Employees = new List<Employee>();
+
+        Departament selectedDepartameent = new Departament();
+        Employee selectedEmployee = new Employee();
 
         public MainWindow()
         {
-            InitializeComponent();
             
+            InitializeComponent();            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ComboBoxDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Departaments.Add(new Departament { Name = "Бухгалтерия" });
         }
     }
 }
